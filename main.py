edfa6b394997548f7cb236c089475e008bead7ae
byb-8 <byb7233@naver.com>
2025-03-10 23:13:26 +0900

    chore: 학습률 수정

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from dataloader import get_dataloader, get_dataset, get_transform
 from model import ConvolutionNet
 from trainer import test, train
 from torch.utils.tensorboard import SummaryWriter
+
 def main():
     transform = get_transform()
     train_dataset, valid_dataset, test_dataset = get_dataset(transform)
     train_loader, valid_loader, test_loader = get_dataloader(train_dataset, valid_dataset, test_dataset, 64)
     model = ConvolutionNet()
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     writer = SummaryWriter()
     train(optimizer, train_loader, model, criterion, 32, valid_loader, writer)
     test(model, test_loader, writer)
